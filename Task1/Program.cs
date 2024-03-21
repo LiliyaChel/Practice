ee4c32fb56ac1f24521b1afb5374d115b44e27a2
Chelishcheva L.D. <chel@lildmit.ru>
2024-03-21 12:26:44 +0300

    Task1 from Ethernal contest

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] abcd = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int result = (abcd[1] >= abcd[3]) ? abcd[0] : abcd[0] + (abcd[3] - abcd[1]) * abcd[2];
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
