3be3f1d4a5f0d11e4ad00a867a4e7bc9cc74bbb4
Chelishcheva L.D. <chel@lildmit.ru>
2024-04-01 00:25:42 +0300

    Task 4 done!

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,7 +9,8 @@ namespace ConsoleApp2
         {
             //Task_1();
             //Task_2();
-            Task_3();
+            //Task_3();
+            Task_4();
             //Console.ReadKey();
         }
 
@@ -40,6 +41,45 @@ namespace ConsoleApp2
                 Console.WriteLine(persones[nt[0] - 1] - 2 * persones[0] + left);
             else
                 Console.WriteLine(2 * persones[nt[0] - 1] - persones[0] - left);
+        }
+
+        static void Task_4()
+        {
+            int[] nk = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            long[] A = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            long start_sum = A.Sum();
+            int curr_point = 0;
+            int[] current = new int[nk[0]];
+            for (int i = 0; i < nk[0]; i++)
+            {
+                curr_point = curr_point > A[i].ToString().Length ? curr_point : A[i].ToString().Length;
+                current[i] = 9;
+            }
+            int k = nk[1];
+            while (curr_point > 0)
+            {
+                for (int i = 0; i < nk[0]; i++)
+                {
+                    if (A[i].ToString().Length >= curr_point)
+                    {
+                        current[i] = int.Parse(A[i].ToString()[A[i].ToString().Length - curr_point].ToString());
+                    }
+                }
+                Array.Sort(current, A);
+                for (int i = 0; i < nk[0]; i++)
+                {
+                    if (current[i] == 9) break;
+                    char[] fix = A[i].ToString().ToArray();
+                    fix[A[i].ToString().Length - curr_point] = '9';
+                    A[i] = long.Parse(new string(fix));
+                    k--;
+                    if (k == 0) break;
+                }
+                if (k == 0) break;
+                curr_point--;
+            }
+            long fin_sum = A.Sum();
+            Console.WriteLine((fin_sum - start_sum));
         }
     }
 }
